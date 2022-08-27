33b41cf971cc9d82a002d99a18b9fb8c1f2d82c0
towakey <kyosgame@gmail.com>
2022-08-27 17:25:26 +0900

    cgiを実行できるように変更

--- ruby_offline_server.rb
+++ ruby_offline_server.rb
@@ -1,8 +1,21 @@
 require 'webrick'
+include WEBrick
+module WEBrick::HTTPServlet
+    FileHandler.add_handler('rb', CGIHandler)
+end
 
 # root = File.expand_path '~/public_html'
 root = Dir.pwd + '/public_html'
-server = WEBrick::HTTPServer.new :Port => 8000, :DocumentRoot => root
+opts = {
+    :Port => 8000,
+    :DocumentRoot => root,
+    :CGIInterpreter => ''
+}
+server = WEBrick::HTTPServer.new(opts)
+# server = WEBrick::HTTPServer.new :Port => 8000, :DocumentRoot => root
+
+# server.mount('/cgi', WEBrick::HTTPServlet::CGIHandler, 'cgi.rb')
+server.mount('/cgi', WEBrick::HTTPServlet::FileHandler, Dir.pwd+'/cgi', {:FancyIndexing => true})
 
 trap 'INT' do server.shutdown end
 server.start
