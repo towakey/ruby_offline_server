ca5df8c9bee6ad5ff32c275fc32d96303376acb1
towakey <kyosgame@gmail.com>
2022-08-29 01:55:22 +0900

    jsonファイルに書き込めるように修正

--- cgi/memo/index.rb
+++ cgi/memo/index.rb
@@ -17,23 +17,44 @@ memoFile = 'memo.json'
 
 # ƒNƒGƒŠ‚ª‚ ‚Á‚½ê‡‚Ìˆ—
 if cgi['q']!='' then
+    content = {}
+    memoJson = {}
     if File.file?(memoFile) then
         memoJson = File.open(memoFile) do |file|
-            JSON.load(memoJson)
+            JSON.load(file)
         end
-    end
-    memoJson[SecureRandom.uuid]['contents'] = cgi['q']
-    open(memoFile, "a") do |file|
-        file.puts(JSON.pretty_generate(memoJson))
+        content = {"contents" =>cgi['q']}
+        # memoJson[SecureRandom.uuid] = content
+        memoJson.store(SecureRandom.uuid, content)
+        open(memoFile, "w") do |file|
+            file.puts(JSON.pretty_generate(memoJson))
+        end
+    else
+        content = {"contents" =>cgi['q']}
+        memoJson[SecureRandom.uuid] = content
+        open(memoFile, "w") do |file|
+            file.puts(JSON.pretty_generate(memoJson))
+        end
     end
 end
 
 
 if File.file?(memoFile) then
+    memoJson = {}
+    memoJson = File.open(memoFile) do |file|
+        JSON.load(file)
+    end
+pu = <<"EOS"
+<hr>
+EOS
+    # puts memoJson
+    memoJson.each{|key, value|
+        pu = pu + "#{value['contents']}<hr>"
+    }
 else
 pu = <<"EOS"
 <hr>
 Not found #{memoFile}
 EOS
-puts pu
 end
+puts pu
